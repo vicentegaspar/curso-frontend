9f1d999ad93745e4d378c3f27ccd20753d531d97
Kodashin <kodashin2@gmail.com>
2022-09-04 17:22:01 -0300

    exercicio modulo 25

---
+++ Modulo 25/javascript-main/src/Animal.js
@@ -0,0 +1,19 @@
+export default class Animal {
+
+    constructor(especie){
+        this.especie = especie
+    }
+
+    falar(){
+        console.log(this.especie + ' fala ')
+    }
+
+    comer(){
+        console.log(this.especie + ' come ')
+    }
+
+    dormir(){
+        console.log(this.especie + ' dorme ')
+    }
+
+}
---
+++ Modulo 25/javascript-main/src/Cachorro.js
@@ -0,0 +1,12 @@
+import Animal from './Animal'
+
+
+class Cachorro extends Animal{
+    falar(){
+        console.log(this.especie + ' fala au au au')
+    }
+
+    comer(){
+        console.log(this.especie + ' come ração ')
+    }
+}
