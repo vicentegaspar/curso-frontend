8684c2163ba9dec5203153e2cc2af8ea72e15300
Kodashin <kodashin2@gmail.com>
2022-09-29 15:37:13 -0300

    exercicio modulo 32

---
+++ Modulo 32/style-guide-main/src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard title="Meu Painel de Controle" />
+    </div>
+  );
+}
+
+export default App;
